88f4a074b89fcbf74d3c658d80aa911773b19812
Rajesh Khoksi <rjshkhoksi26@gmail.com>
2022-09-17 23:12:14 +0600

    return value

---
+++ returnFunction.js
@@ -0,0 +1,13 @@
+const firstName = 'Rajehs'
+const lastName = "Khoksi"
+
+const returnFunction =()=>{
+    
+    if(firstName==='Rajehs'){
+        return lastName;
+    }
+    console.log(firstName)
+}
+
+const name=returnFunction();
+console.log(name);
